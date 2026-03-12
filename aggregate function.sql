00f4f23ee2448bdcf8e5b13bf7f31914d424d880
ft262ddj62-ship-it <kamila_azamatova@mail.ru>
2026-03-12 13:10:11 +0300

    Create aggregate function.sql
    Показывает:
    - количество заказов
    - выручку
    - средний чек по городам

---
+++ aggregate function.sql
@@ -0,0 +1,10 @@
+SELECT 
+c.city,
+COUNT(o.order_id) AS total_orders,
+SUM(o.total_amount) AS total_revenue,
+AVG(o.total_amount) AS avg_order_value
+FROM customers c
+JOIN orders o 
+ON c.customer_id = o.customer_id
+GROUP BY c.city
+ORDER BY total_revenue DESC;
